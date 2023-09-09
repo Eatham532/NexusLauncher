43a59e22aa1b6cc11f50e7c784955f9483516c20
Eatham <eatham532@gmail.com>
2023-09-09 14:14:58 +1000

    Initial commit

---
+++ src/components/InstanceView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const props = defineProps(['width, height']);
+
+</script>
+
+<template>
+  <button class="InstanceViewBtn">
+    Instance
+  </button>
+</template>
+
+<style scoped>
+  .InstanceViewBtn {
+    border-radius: 10px;
+    margin: 20px;
+    aspect-ratio: 1;
+    width: 200px;
+  }
+</style>
